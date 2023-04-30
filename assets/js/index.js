fc93146dcc56d6a987ebd68b696205b73d2ff2a0
anthonyhaj <>
2023-04-30 18:52:38 +0000

    add additonal const varibales to select html ids

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,12 @@ let playerScore = 0;
 let computerScore = 0;
 
 // Get references to the relevant HTML elements
-const rockButtton = document.getElementById(rock-button);
+const rockButtton = document.getElementById("rock-button");
+const paperButton = document.getElementById("paper-button");
+const scissorsButton = document.getElementById("scissors-button");
+const playAgainButton = document.getElementById("play-again-button");
+
+const playerScoreDisplay = document.getElementById("p-score");
+const computerScoreDisplay = document.getElementById("c-score");
+const resultDisplay = document.getElementById("result-text");
+const computerMoveImg = document.querySelector('.computer-move img');
