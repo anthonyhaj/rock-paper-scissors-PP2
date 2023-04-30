e34b89eae14d1ec91a4b162934ea98a8c05b666c
anthonyhaj <>
2023-04-30 18:41:44 +0000

    add first const variable for rock button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,6 @@
 // Define global variables to keep track of the game state
 let playerScore = 0;
 let computerScore = 0;
+
+// Get references to the relevant HTML elements
+const rockButtton = document.getElementById(rock-button);
