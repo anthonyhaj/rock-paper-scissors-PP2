47cfb50ea897f1f8474b43087dcb53f7d4ad9ff2
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-05-02 09:45:17 +0000

    add animation for computer move img in js

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,7 +19,7 @@ const playerMoveImg = document.querySelector('.you-chose img');
 // Function to generate random computer move
 function getComputerMove() {
     const moves = ['rock', 'paper', 'scissors'];
-    const randomIndex = Math.floor (Math.random() * moves.length);
+    const randomIndex = Math.floor(Math.random() * moves.length);
     return moves[randomIndex];
 }
 
@@ -52,67 +52,76 @@ function updatePlayerMoveImage(playerMove) {
 // Function to play a single round of the game
 function playRound(playerMove) {
     const computerMove = getComputerMove();
-    updateComputerMoveImage(computerMove);
+    // Animation to switch between possible moves images"
+    let i = 0; // Variable to keep track of the current index in the animation array
+    let animation = ['rock.png', 'paper.png', 'scissors.png']; // Array of image names for the animation
+    const animationInterval = setInterval(() => {
+            computerMoveImg.src = './assets/images/' + animation[i];
+            i++;
+            if (i >= animation.length) {
+                clearInterval(animationInterval); // Stop the animation when it reaches the end of the array
 
-    if (playerMove === computerMove) {
-        resultDisplay.textContent = "It's a Tie!"
-    } else if (
-        (playerMove === 'rock' && computerMove === 'scissors') ||
-        (playerMove === 'paper' && computerMove === 'rock') ||
-        (playerMove === 'scissors' && computerMove === 'paper')
-    ) {
-        resultDisplay.textContent = 'You Win!';
-        playerScore++;
-        playerScoreDisplay.textContent = playerScore;
-    } else {
-        resultDisplay.textContent = 'Computer Wins!';
-        computerScore++;
-        computerScoreDisplay.textContent = computerScore;
+                updateComputerMoveImage(computerMove);
+
+                if (playerMove === computerMove) {
+                    resultDisplay.textContent = "It's a Tie!"
+                } else if (
+                    (playerMove === 'rock' && computerMove === 'scissors') ||
+                    (playerMove === 'paper' && computerMove === 'rock') ||
+                    (playerMove === 'scissors' && computerMove === 'paper')
+                ) {
+                    resultDisplay.textContent = 'You Win!';
+                    playerScore++;
+                    playerScoreDisplay.textContent = playerScore;
+                } else {
+                    resultDisplay.textContent = 'Computer Wins!';
+                    computerScore++;
+                    computerScoreDisplay.textContent = computerScore;
+                }
+
+                updatePlayerMoveImage(playerMove);
+
+                if (round === 10) {
+                    if (playerScore > computerScore) {
+                        resultDisplay.textContent = 'You win the game!'
+                    } else if (computerScore > playerScore) {
+                        resultDisplay.textContent = 'You lose the game!'
+                    } else {
+                        resultDisplay.textContent = "It's a tie game!"
+                    }
+                    rockButton.disabled = true;
+                    paperButton.disabled = true;
+                    scissorsButton.disabled = true;
+                } else {
+                    round++;
+                }
+            }
+        }, 300); // Change the interval duration (in milliseconds) to adjust the animation speed
     }
 
-    updatePlayerMoveImage(playerMove);
-    
-    if (round === 10) {
-        if (playerScore > computerScore) {
-            resultDisplay.textContent = 'You win the game!'
-        } else if (computerScore > playerScore) {
-            resultDisplay.textContent = 'You lose the game!'
-        } else {
-            resultDisplay.textContent = "It's a tie game!"
-        }
-        rockButton.disabled = true;
-        paperButton.disabled = true;
-        scissorsButton.disabled = true;
-    } else {
-        round++;
+    // Function to reset game state
+    function resetGame() {
+        playerScore = 0;
+        computerScore = 0;
+        round = 1;
+        playerScoreDisplay.textContent = 0;
+        computerScoreDisplay.textContent = 0;
+        rockButton.disabled = false;
+        paperButton.disabled = false;
+        scissorsButton.disabled = false;
     }
-}
 
-// Function to reset game state
-function resetGame() {
-    playerScore = 0;
-    computerScore = 0;
-    round = 1;
-    playerScoreDisplay.textContent = 0;
-    computerScoreDisplay.textContent = 0;
-    rockButton.disabled = false;
-    paperButton.disabled = false;
-    scissorsButton.disabled = false;
-}
+    // Event listeners for player moves
 
-// Event listeners for player moves
-
-rockButton.addEventListener('click', function () {
-    playRound('rock');
-});
-paperButton.addEventListener('click', function () {
-    playRound('paper');
-});
-scissorsButton.addEventListener('click', function () {
-    playRound('scissors');
-});
-playAgainButton.addEventListener('click', function () {
+    rockButton.addEventListener('click', function () {
+        playRound('rock');
+    });
+    paperButton.addEventListener('click', function () {
+        playRound('paper');
+    });
+    scissorsButton.addEventListener('click', function () {
+        playRound('scissors');
+    });
+    playAgainButton.addEventListener('click', function () {
         resetGame();
-});
-
-
+    });
